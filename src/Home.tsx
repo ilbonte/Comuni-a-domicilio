92e2ef5e908f4c3678704bf09a4ce656becbbb98
Davide Bontempelli <davide.bonte@gmail.com>
2020-04-08 00:26:03 +0200

    add homepage and route for castiglione

---
+++ src/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default class Home extends React.PureComponent {
+  render() {
+    return (
+        <div>
+            <h1>Comuni a domicilio</h1>
+            <h2>Pagina in costruzione</h2>
+            <ul className="navbar-nav mr-auto">
+                <li><a href="/">Home </a></li>
+                <li><a href="/castiglione-delle-stiviere">Castiglione delle Stiviere</a></li>
+            </ul>
+        </div>
+    );
+  }
+}

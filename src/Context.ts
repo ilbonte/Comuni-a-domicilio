9fc904269938c2e257d4da3e7c59cb196e589363
Davide Bontempelli <davide.bonte@gmail.com>
2020-04-09 00:46:40 +0200

    add grosseto, misano and molinella

--- src/Context.ts
+++ src/Context.ts
@@ -8,4 +8,26 @@ export const MunicipalitiesContext = React.createContext([
         province: 'Mantova',
         formUrl: 'https://docs.google.com/forms/d/e/1FAIpQLSdci-4udC2gyI2wZcZcsa8nEStyQ0Ga0PuzjtPADBYtlKOsrg/viewform'
     },
+    {
+        id: 'AKfycbzqpH3Ljir5HtI_Mn8Iy0K6QUGvUdEuqNJpq-3d3Q',
+        slug: 'grosseto',
+        name: 'Grosseto',
+        province: 'Grosseto',
+        formUrl: 'https://forms.gle/WXNCBpcdvXxs8Ut49'
+    },
+    {
+        id: 'AKfycbxA4oCL0500HHUFLMV8EvgVpGLtuJc0HgdqMT4MvCxBrB0ePlg',
+        slug: 'misano-adriatico',
+        name: 'Misano Adriatico',
+        province: 'Rimini',
+        formUrl: 'https://forms.gle/JTpqjSqbA6zCycr57'
+    },
+    {
+        id: 'AKfycbym7aR1Ew3JXas2jyjf0Zk6xo59kZs91FV85RcP7w',
+        slug: 'molinella',
+        name: 'Molinella',
+        province: 'Bologna',
+        formUrl: 'https://forms.gle/JHAEgi73udZqtAhy7'
+    },
+
 ]);

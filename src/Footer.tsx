22a65046a73bee369118e3f3cc1c32036ecbf45f
tia <larentis@spaziodati.eu>
2020-04-08 22:27:09 +0200

    fix footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export var Footer: React.FC<{}> = (props): React.ReactElement => {
     return (
-        <footer className="border-top pt-4 mt-4 mb-4">
+        <footer className="border-top pt-4 mt-4 mb-4 text-left">
             Fatto con <i className="fa fa-heart" /> e con la mascherina da
             Davide Bontempelli, Mattia Larentis e Marco Musi.
         </footer>
